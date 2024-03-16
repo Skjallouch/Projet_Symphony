df8f47239286cc0ce5031b957f20239b46dbef86
Sara Kjallouch <skjallouch@yahoo.com>
2024-03-16 14:48:13 +0100

    Recette Maison + caroussel

--- src/Controller/RecetteMaisonController.php
+++ src/Controller/RecetteMaisonController.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,3 +16,42 @@ class RecetteMaisonController extends AbstractController
         ]);
     }
 }
+//avant le test pour carroussel
+*/
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Finder\Finder;
+
+// Assurez-vous d'inclure le composant Finder
+
+class RecetteMaisonController extends AbstractController
+{
+    #[Route('/recettes', name: 'app_recette_maison')]
+    public function index(): Response
+    {
+        $projectDir = $this->getParameter('kernel.project_dir');
+        $imagesDir = $projectDir . '/public/images/recette_caroussel';
+
+        $finder = new Finder();
+        $finder->files()->in($imagesDir);
+
+        $images = [];
+        foreach ($finder as $file) {
+            $images[] = 'images/recette_caroussel/' . $file->getRelativePathname();
+        }
+
+        return $this->render('features/recette_maison.html.twig', [
+            'controller_name' => 'RecetteMaisonController',
+            'photos_caroussel' => $images
+        ]);
+    }
+}
+
+
+
+
