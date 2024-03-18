e06876eb1743a7fe3b557af960492317fc5987d4
teodoraistrate <teodoraistrate63@yahoo.com>
2024-03-18 12:48:29 +0100

    New Entity (Advice) for the page Conseils de grand-mere

---
+++ src/Entity/Advice.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdviceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdviceRepository::class)]
+class Advice
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $advice = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAdvice(): array
+    {
+        return $this->advice;
+    }
+
+    public function setAdvice(array $advice): static
+    {
+        $this->advice = $advice;
+
+        return $this;
+    }
+}
