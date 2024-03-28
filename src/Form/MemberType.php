e1c6d5709b6606e4f1d195a683718531aaa3a2d4
teodoraistrate <teodoraistrate63@yahoo.com>
2024-03-28 19:04:24 +0100

    CRUD Member

---
+++ src/Form/MemberType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\BlogArticle;
+use App\Entity\Member;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MemberType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('password')
+            ->add('viewedArticles', EntityType::class, [
+                'class' => BlogArticle::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Member::class,
+        ]);
+    }
+}
