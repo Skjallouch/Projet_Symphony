adda418211f752fd1d2bf966c85c292a87609a75
teodoraistrate <teodoraistrate63@yahoo.com>
2024-03-30 16:18:30 +0100

    formulaire creation compte - plus beau

--- src/Form/MemberType.php
+++ src/Form/MemberType.php
@@ -6,8 +6,12 @@ use App\Entity\BlogArticle;
 use App\Entity\Member;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class MemberType extends AbstractType
 {
@@ -17,15 +21,25 @@ class MemberType extends AbstractType
             ->add('firstName')
             ->add('lastName')
             ->add('email')
-            ->add('password')
-            /*
-            ->add('viewedArticles', EntityType::class, [
-                'class' => BlogArticle::class,
-'choice_label' => 'id',
-'multiple' => true,
-            ])
-            */
-        ;
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
