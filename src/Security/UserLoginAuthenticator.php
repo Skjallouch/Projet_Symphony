5518f2621f22fb133cb19c3f60183259fdb7c140
Sara Kjallouch <skjallouch@yahoo.com>
2024-03-30 12:47:25 +0100

    Auth à debugger

--- src/Security/UserLoginAuthenticator.php
+++ src/Security/UserLoginAuthenticator.php
@@ -7,29 +7,51 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+
 
 class UserLoginAuthenticator extends AbstractAuthenticator
 {
+    private UrlGeneratorInterface $urlGenerator;
+
     public function supports(Request $request): ?bool
     {
-        // TODO: Implement supports() method.
+        return $request->attributes->get('_route') === 'app_login' && $request->isMethod('POST');
     }
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
 
     public function authenticate(Request $request): Passport
     {
-        // TODO: Implement authenticate() method.
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($password)
+        );
     }
+
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        $targetUrl = $this->urlGenerator->generate('app_home');
+        return new RedirectResponse($targetUrl);
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
+        $message = strtr($exception->getMessageKey(), $exception->getMessageData());
+        return new RedirectResponse($this->urlGenerator->generate('app_login', ['error' => $message]));
     }
+
 
     //    public function start(Request $request, AuthenticationException $authException = null): Response
     //    {
